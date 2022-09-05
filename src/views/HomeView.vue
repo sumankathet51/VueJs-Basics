7797e6b82bf90cf7fa77f27a462acf4bc386b49d
Suman Kathet <sumankathet51@gmail.com>
2022-09-05 16:59:50 +0545

    basic Hello World program

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,14 +6,6 @@ const dataStore = useCounterStore();
 
 <template>
   <main>
-    <h1>{{ dataStore.msg.toUpperCase() }}</h1>
-    <input v-model="dataStore.count" type="number" />
-    <div class="count">{{ dataStore.count }}</div>
-    <div class="button">
-      <button @click="dataStore.addCount(1)">+</button>
-      <button @click="dataStore.addCount(-1)">-</button>
-    </div>
-    <hr />
-    <div>{{ dataStore.oddOrEven }}</div>
+    <h1>Hello World</h1>
   </main>
 </template>
